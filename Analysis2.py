2fd540519a8e54b9c6d859e8f88a9fb119f8802c
unknown <g00048625@gmit.ie>
2021-03-29 21:07:06 +0100

    sklearndatasets

---
+++ Analysis2.py
@@ -0,0 +1,9 @@
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+print(iris)
+
+
+
+# ref : https://en.wikipedia.org/wiki/Iris_flower_data_set#:~:text=The%20Iris%20flower%20data%20set,example%20of%20linear%20discriminant%20analysis.
+# Due to the fact the Iris Data Set is so often analysed using python is it actually built in to the sklearn datasets. The dataset can be printed out as a data array set.
